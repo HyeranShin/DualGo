bd1f6a73d972e81da57fa6e2fc67e1aa23c9e26d
HyeranShin <hyebaragi__@naver.com>
2018-11-30 17:40:20 +0900

    Add scenes, fonts and Polish views

---
+++ DualGo/Assets/Scripts/MoveScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScene : MonoBehaviour {
+    public string SceneName;
+     
+	public void onClick()
+    {
+        Application.LoadLevel(SceneName);
+    }
+}
