3b538ca03358c2af9d00d3919746ea494fece577
yewonjo97 <36885223+yewonjo97@users.noreply.github.com>
2018-12-04 21:29:17 +0900

    button&rule
    backButton Event
    rule change

--- DualGo/Assets/Scripts/MoveScene.cs
+++ DualGo/Assets/Scripts/MoveScene.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MoveScene : MonoBehaviour {
     public string SceneName;
      
 	public void onClick()
     {
-        Application.LoadLevel(SceneName);
+        SceneManager.LoadScene (SceneName);
     }
 }
---
+++ DualGo/Assets/Scripts/RuleManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleManager : MonoBehaviour {
+    public GameObject rule1;
+    public GameObject rule2;
+    public GameObject rule3;
+
+	// Use this for initialization
+	void Start () {
+        rule2.SetActive(false);
+        rule3.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rule1.SetActive(false);
+            rule2.SetActive(true);
+            /*여기서 rule2 -> 3로 넘어가는 부분
+            if (rule2.activeSelf == true && Input.GetMouseButton(0))
+            {
+                rule2.SetActive(false);
+                rule3.SetActive(true);
+            }
+            */
+        }        
+        
+	}
+ 
+}
