14f10318ed89b0f9eab9c75d4d99d7914ab4cc42
HyeranShin <hyebaragi__@naver.com>
2018-12-05 14:54:41 +0900

    Add ScoreManager.cs

---
+++ DualGo/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    public GameObject score;
+    public GameObject multiple;
+
+	// Use this for initialization
+	void Start () {
+        ///////////////////혜란//////////////////////
+
+        ///////////////////예원//////////////////////
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        ///////////////////혜란//////////////////////
+
+        ///////////////////예원//////////////////////
+
+    }
+
+
+    ///////////////////혜란//////////////////////
+
+    ///////////////////예원//////////////////////
+
+}
